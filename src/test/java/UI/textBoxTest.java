dff98ed17b226a1cd67cd2858d7ba94f635e7195
Марина Миловкина <un6un5@gmail.com>
2025-04-16 02:58:13 +0300

    ÐдДобавлены комментарии

--- src/test/java/UI/textBoxTest.java
+++ src/test/java/UI/textBoxTest.java
@@ -1,7 +1,7 @@
-package UI;
+package UI; // указывает, что класс textBoxTest находится в пакете UI. Это помогает организовать проект
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By; // импорт классов для работы с браузером (WebDriver,By,WebElement), библиотекой TestNG
+import org.openqa.selenium.WebDriver; // (аннотации @Test, @BeforeClass и т.д.), менеджером драйверов и пр.
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -20,42 +20,50 @@ public class textBoxTest {
 
     public WebDriver driver;
 
-    @BeforeClass
-    public void setup() {
+    @BeforeClass // метод запускается один раз перед всеми тестами
+    public void setup() { // настройка браузера
         WebDriverManager.chromedriver().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setCapability(CapabilityType.PAGE_LOAD_STRATEGY,"eager");
+        chromeOptions.setCapability(CapabilityType.PAGE_LOAD_STRATEGY,"eager"); // браузер не будет ждать полной загрузки всех ресурсов (картинок, например)
         driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
     }
 
-    @AfterClass
+    @AfterClass // выполняется один раз после всех тестов
     public void tearDown() {
         driver.quit();
-    }
+    } // закрытие браузера после всех тестов
 
+    // основной тест: проверка формы
     @Test
     public void checkTextBox() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/text-box");
+        // WebDriver driver = new ChromeDriver();
+        this.driver.get("https://demoqa.com/text-box"); // открытие страницы
 
+        // ввод данных в поля
+        // заполнение поля Full Name
         WebElement userNameInput = driver.findElement(By.xpath("//input[@id='userName']"));
         userNameInput.sendKeys("Marina");
 
+        // заполнение поля Email
         WebElement userEmailInput = driver.findElement(By.xpath("//input[@id='userEmail']"));
         userEmailInput.sendKeys("mar@yandex.ru");
 
+        // заполнение поля Current Address
         WebElement currentAddressTextArea = driver.findElement(By.xpath("//textarea[@id='currentAddress']"));
         currentAddressTextArea.sendKeys("Kursk");
 
+        // заполнение поля Permanent Address
         WebElement permanentAddressTextArea = driver.findElement(By.xpath("//textarea[@id='permanentAddress']"));
         permanentAddressTextArea.sendKeys("Kursk");
 
+        // прокрутка страницы до кнопки submit
         WebElement button = driver.findElement(By.xpath("//button[@id='submit']"));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView();", button);
-        button.click();
+        button.click(); // нажатие на кнопку
 
+        // получаем отображенные данные
         WebElement userNameOutput = driver.findElement(By.xpath("//p[@id='name']"));
         WebElement userEmailOutput = driver.findElement(By.xpath("//p[@id='email']"));
         WebElement currentAddressOutput = driver.findElement(By.xpath("//p[@id='currentAddress']"));
@@ -66,17 +74,20 @@ public class textBoxTest {
         String currAddress = currentAddressOutput.getText();
         String permAddress = permanentAddressOutput.getText();
 
+        // пауза (необязательно)
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-            System.out.println("Name: " + name + " , email: " + email
+        // вывод на консоль
+        System.out.println("Name: " + name + " , email: " + email
                     + " , current address: " + currAddress + " , permanent address: " + permAddress);
 
-           // Assert.assertTrue(name.contains("Marina"));
-            Assert.assertEquals(email, "Email:mar@yandex.ru");
+        // проверка поля Email
+        // Assert.assertTrue(name.contains("Marina"));
+        Assert.assertEquals(email, "Email:mar@yandex.ru");
         }
 
     @Test
